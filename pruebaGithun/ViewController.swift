62e069bbc694356c7f94e799622e3179e89f2e19
Diego Herrera <reeezak@gmail.com>
2015-10-24 17:45:11 -0500

    Initial Commit

---
+++ pruebaGithun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGithun
+//
+//  Created by Diego Herrera on 10/24/15.
+//  Copyright © 2015 Diego Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ pruebaGithunTests/pruebaGithunTests.swift
@@ -0,0 +1,36 @@
+//
+//  pruebaGithunTests.swift
+//  pruebaGithunTests
+//
+//  Created by Diego Herrera on 10/24/15.
+//  Copyright © 2015 Diego Herrera. All rights reserved.
+//
+
+import XCTest
+@testable import pruebaGithun
+
+class pruebaGithunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
